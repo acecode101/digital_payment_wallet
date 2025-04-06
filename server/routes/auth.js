0faf5000d472bb5fc91532f8bce81fafd84f5991
acecode-01 <154255373+acecode-01@users.noreply.github.com>
2025-04-07 01:35:44 +0530

    Fixed bcrypt issue by switching to bcryptjs

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // ✅ FIXED HERE
 const User = require('../models/user');
 const router = express.Router();
 
